e60ea9eb1878567b4ad32d55420b853f2878e7fb
Rose Aravena Madariaga <rosearavenamadariaga@MacBook-Air-de-Rose.local>
2022-10-17 17:03:37 -0300

    add react big calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,54 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { addHours } from 'date-fns';
+
 import { Navbar } from "../"
+import { localizer, getMessagesES } from '../../helpers';
+
+
+const events = [{
+    title: 'Mi cumplaeños',
+    notes: 'comprar torta',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'Rose'
+    }
+}]
 
 export const CalendarPage = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        console.log({event, start, end, isSelected})
+
+        const style = {
+            backgroundColor: '#347CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+
     return (
         <>
             <Navbar />
+
+            <Calendar
+                culture='es'
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+                messages={getMessagesES()}
+                eventPropGetter={eventStyleGetter}
+            />
         </>
     )
 }
