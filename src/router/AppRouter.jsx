30f33c622a49bdf6c524283a6bd6bed3a0a21e09
Rose Aravena Madariaga <rosearavenamadariaga@MacBook-Air-de-Rose.local>
2022-10-17 16:24:46 -0300

    template loginPage and navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import { CalendarPage } from "../calendar"
 
 export const AppRouter = () => {
 
-    const authStatus = 'not-authenticated'
+    const authStatus = 'authenticated'
 
     return (
         <Routes>
