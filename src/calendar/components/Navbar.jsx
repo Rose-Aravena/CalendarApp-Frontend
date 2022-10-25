2c9675167c92063ea9241e13ac60793d96386668
Rose Aravena Madariaga <rosearavenamadariaga@MacBook-Air-de-Rose.local>
2022-10-25 19:55:03 -0300

    login and logout

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,14 +1,19 @@
+import { useAuthStore } from "../../hooks"
 
 export const Navbar = () => {
+
+    const {startLogout, user} = useAuthStore()
+
     return (
         <div className="navbar navbar-dark bg-dark mb-4 px-4">
             <span className="navbar-brand">
                 <i className="fas fa-calendar-alt"></i>
                 &nbsp;
-                Rose
+                {user.name}
             </span>
-            <button className="btn btn-outline-danger">
+            <button onClick={ startLogout } className="btn btn-outline-danger">
                 <i className="fas fa-sign-out-alt"></i>
+                &nbsp;
                 <span>Salir</span>
             </button>
         </div>
