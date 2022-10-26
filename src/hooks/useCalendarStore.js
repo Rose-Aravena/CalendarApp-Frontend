41cd7f08bafa5069a45ff4cbe5e38d838c48c020
Rose Aravena Madariaga <rosearavenamadariaga@MacBook-Air-de-Rose.local>
2022-10-26 11:29:07 -0300

    create event and save it to DB

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,20 +1,24 @@
 import { useDispatch, useSelector } from "react-redux"
+import { calendarApi } from "../api";
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
 
 export const useCalendarStore = () => {
 
     const dispatch = useDispatch();
     const {events, activeEvent} = useSelector( state => state.calendar );
+    const {user} = useSelector( state => state.auth );
 
     const setActiveEvent = (calendarEvent) => {
         dispatch(onSetActiveEvent(calendarEvent))
     }
 
     const startSavingEvent = async(calendarEvent) => {
+
         if(calendarEvent._id){
             dispatch(onUpdateEvent({...calendarEvent}));
         } else {
-            dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime()}));
+            const {data} = await calendarApi.post('/events', calendarEvent);
+            dispatch(onAddNewEvent({...calendarEvent, id: data.event.id, user}));
         }
     }
 
